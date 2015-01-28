53896912aace2bf4242e816c0028efa2f85d26e1
danielronnkvist <daniel@ronnkvist.com>
2015-01-28 08:16:01 +0100

    Started working on rules to calculate the rotors speed

--- main.m
+++ main.m
@@ -1,16 +1,55 @@
-g = 9.82;
-m = 1;
+clear all
+s = 1000;
+g(1:s) = 9.82;
+m(1:s) = 1;
 r = 0.5;
 A = pi*r^2;
 p = 1.2;
 
-h0 = 0;
-hf = 2.5;
-theta = 30;
-phi = 25;
+h0(1:s) = 0;
+hf = linspace(0, 1, s);
+theta = linspace(0, 0, s);
+phi = linspace(0, 45, s);
 
+T  = (p*A*4*g.*(hf-h0) + m.*g)./(cosd(theta).*cosd(phi))
+Tx = sqrt(-abs(T).^2.*cosd(theta).^2.*(1-1./(cosd(theta).^2)))
+Ty = abs(T).*cosd(theta).*sind(phi)
+Tz = abs(T).*cosd(theta).*cosd(phi)
 
-T  = (p*A*4*g*(hf-h0) + m*g)/(cos(theta)*cos(phi))
-Tx = sqrt(-abs(T)^2*cos(theta)^2*(1-1/(cos(theta)^2)))
-Ty = abs(T)*cos(theta)*sin(phi)
-Tz = abs(T)*cos(theta)*cos(phi)
+%ratio = theta/phi;
+%Tphi = T/(1 + ratio)
+%Ttheta = ratio*Tphi
+
+plot3(Tx, Ty, Tz)
+
+% if theta == 0 && phi == 0
+%     T1 = Tz/4
+%     T2 = -Tz/4
+%     T3 = -Tz/4
+%     T4 = Tz/4
+% elseif phi == 0
+%     if theta > 0 && theta < 90
+%         T1 = Tz/4
+%         T2 = -Tz/4
+%         T3 = -Tz/4
+%         T4 = (T - Tz)+(Tz/4)
+%     elseif theta < 0 && theta > -90
+%         T1 = Tz/4
+%         T2 = -Tz/4
+%         T3 = -Tz/4
+%         T4 = (T - Tz)+(Tz/4)
+%     end
+% elseif theta == 0
+%     if phi > 0 && phi < 90
+%         T1 = Tz/4
+%         T2 = -(T - Tz)+(Tz/4)
+%         T3 = -Tz/4
+%         T4 = Tz/4
+%     elseif phi < 0 && phi > -90
+%         T1 = Tz/4
+%         T2 = -(T - Tz)+(Tz/4)
+%         T3 = -Tz/4
+%         T4 = Tz/4
+%     end
+% end
+
