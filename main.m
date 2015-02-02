59ab1f2df2a0d2211a21b63bbc11bbfb7047ad13
danielronnkvist <daniel@ronnkvist.com>
2015-02-02 09:52:09 +0100

    Velocities for each rotor

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 clear all
-s = 1000;
+s = 1;
 g(1:s) = 9.82;
 m(1:s) = 1;
 r = 0.5;
@@ -7,49 +7,23 @@ A = pi*r^2;
 p = 1.2;
 
 h0(1:s) = 0;
-hf = linspace(0, 1, s);
-theta = linspace(0, 0, s);
+hf = linspace(0, 4, s);
+theta = linspace(0, 30, s);
 phi = linspace(0, 45, s);
 
-T  = (p*A*4*g.*(hf-h0) + m.*g)./(cosd(theta).*cosd(phi))
-Tx = sqrt(-abs(T).^2.*cosd(theta).^2.*(1-1./(cosd(theta).^2)))
-Ty = abs(T).*cosd(theta).*sind(phi)
-Tz = abs(T).*cosd(theta).*cosd(phi)
+T  = (p*A*4*g.*(hf-h0) + m.*g)./(cosd(theta).*cosd(phi));
+Tx = sqrt(-abs(T).^2.*cosd(theta).^2.*(1-1./(cosd(theta).^2)));
+Ty = abs(T).*cosd(theta).*sind(phi);
+Tz = abs(T).*cosd(theta).*cosd(phi);
 
-%ratio = theta/phi;
-%Tphi = T/(1 + ratio)
-%Ttheta = ratio*Tphi
+% plot3(Tx, Ty, Tz)
 
-plot3(Tx, Ty, Tz)
+T1 = cosd(theta)*(Tz/4);
+T4 = (Tz/4)*(1 + (1 - cosd(theta)));
+T2 = cosd(phi)*(Tz/4);
+T3 = (Tz/4)*(1 + (1 - cosd(phi)));
 
-% if theta == 0 && phi == 0
-%     T1 = Tz/4
-%     T2 = -Tz/4
-%     T3 = -Tz/4
-%     T4 = Tz/4
-% elseif phi == 0
-%     if theta > 0 && theta < 90
-%         T1 = Tz/4
-%         T2 = -Tz/4
-%         T3 = -Tz/4
-%         T4 = (T - Tz)+(Tz/4)
-%     elseif theta < 0 && theta > -90
-%         T1 = Tz/4
-%         T2 = -Tz/4
-%         T3 = -Tz/4
-%         T4 = (T - Tz)+(Tz/4)
-%     end
-% elseif theta == 0
-%     if phi > 0 && phi < 90
-%         T1 = Tz/4
-%         T2 = -(T - Tz)+(Tz/4)
-%         T3 = -Tz/4
-%         T4 = Tz/4
-%     elseif phi < 0 && phi > -90
-%         T1 = Tz/4
-%         T2 = -(T - Tz)+(Tz/4)
-%         T3 = -Tz/4
-%         T4 = Tz/4
-%     end
-% end
-
+v1 = sqrt(T1/(p*A))
+v2 = sqrt(T2/(p*A))
+v3 = sqrt(T3/(p*A))
+v4 = sqrt(T4/(p*A))
