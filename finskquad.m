12ffa9a7da979a699d886cf06e3a3467d22ef814
tistatos <tistatos@gmail.com>
2015-02-03 16:48:56 +0100

    started adding trajectory control

--- finskquad.m
+++ finskquad.m
@@ -1,34 +1,38 @@
 clear all;
 % position och vinkel i intertial frame
 pos = zeros(3,1);
-ang = zeros(3,1);
-
-% hastigheten för x, y och z i body frame
-v = zeros(3,1);
-% vinkelhastigheten i body frame
-angVelo = zeros(3,1);
-
-gravity = 9.82;
-mass = 1;
-thrust = gravity;
+%acceleration
+acc = zeros(3,1);
 % vinkeln i body frame
-
-ang = [linspace(0,4); linspace(0,10); linspace(0,0)]
+ang = zeros(3,1);
+% hastigheten fï¿½r x, y och z i body frame
+v = zeros(3,1);
+%
+gravity = -9.82;
+%massa
+mass = 1;
+%lyftkraft
+thrust = 0;
+desiredV = [0; 0; 10];
 
 for t=1:100
-    r = rotationMatrix(ang(:,t));
-    r(3,3)
+    d = acc+v./mass;
+    r = rotationMatrix(ang(:));
+    thrust = mass.*d'*r;
+    ang(2) = asind((d(1)*sind(ang(3)) - d(2)*cosd(ang(3))) / ...
+                (d(1).^2+d(2).^2+(d(3)+gravity).^2));
+    ang(1) = acosd((d(1))*cosd(ang(3)) - d(2)*sind(ang(3))/(d(3)+gravity));
+
+
     %     Ekvation 10 i finska rapporten
-    %     thrust är lyftkraften, det är den vi ska styra
+    %     thrust ï¿½r lyftkraften, det ï¿½r den vi ska styra
     acc = -gravity*[0,0,1]' + (thrust/mass)*r(:,3);
-    accel(:,t) = acc;
-    v(:,t) = acc.*t;
-    pos = pos+v(:,t);
+    v = acc.*t;
+    pos = pos+v;
     posSave(:,t) = pos;
 end
-plot(posSave(1,:),'r')
-hold
-plot(posSave(2,:),'g')
-plot(posSave(3,:),'b')
-figure
+% plot(posSave(1,:),'r')
+% hold
+% plot(posSave(2,:),'g')
+% plot(posSave(3,:),'b')
 plot3(posSave(1,:),posSave(2,:),posSave(3,:))
