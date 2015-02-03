c675974ac4be6a37db62a344f61701626f4aa6af
danielronnkvist <daniel@ronnkvist.com>
2015-02-03 13:44:57 +0100

    Added update function to flightControl. Updates the velocities by delta passed from the rendering loop. And some comments.

--- scripts/flightControl.js
+++ scripts/flightControl.js
@@ -1,3 +1,14 @@
+/*
+
+flightControl()
+
+Returns an object consisting of if buttons are pressed and velocities with their value.
+
+The function temp.velocities.decrease(t, axis) decreases the velocity on the axis by 2*t.
+  * axis [string]
+  * t [float]
+
+*/
 function flightControl(){
   var temp = {};
   temp.x   = { pos: false, neg: false };
@@ -23,6 +34,40 @@ function flightControl(){
       }
     }
   };
+
+  /*
+
+  Alters the velocity with delta depending on which buttons are pressed.
+  If no button is pressed on an axis the axis velocity is decreased.
+
+  */
+  temp.update = function(delta) {
+    if(temp.x.neg){
+      temp.velocities.x -= delta;
+    }if(temp.x.pos){
+      temp.velocities.x += delta;
+    }if(!temp.x.pos && !temp.x.neg){
+      temp.velocities.decrease(delta, 'x');
+    }if(temp.y.neg){
+      temp.velocities.y -= delta;
+    }if(temp.y.pos){
+      temp.velocities.y += delta;
+    }if(!temp.y.pos && !temp.y.neg){
+      temp.velocities.decrease(delta, 'y');
+    }if(temp.z.neg){
+      temp.velocities.z -= delta;
+    }if(temp.z.pos){
+      temp.velocities.z += delta;
+    }if(!temp.z.pos && !temp.z.neg){
+      temp.velocities.decrease(delta, 'z');
+    }if(temp.yaw.neg){
+      copter.rotation.y -= 8*delta;
+    }if(temp.yaw.pos){
+      copter.rotation.y += 8*delta;
+    }
+  };
+
+  // Event listeners on keypresses
   document.onkeydown = function(evt) {
       switch(evt.keyCode){
         case 65: /*A*/ temp.x.neg = true; break;
@@ -48,6 +93,8 @@ function flightControl(){
         case 76: /*L*/ temp.yaw.pos = false; break;
       };
   };
+
+  // Reset the copters values
   function reset(){
     copter.position.setX(0);
     copter.position.setY(0);
@@ -55,5 +102,6 @@ function flightControl(){
     copter.rotation.y = 0;
     temp.velocities.x = temp.velocities.y = temp.velocities.z = 0;
   };
+
   return temp;
 }
--- scripts/geometry.js
+++ scripts/geometry.js
@@ -2,11 +2,11 @@ var copterGeometry = new THREE.BoxGeometry( 1, 0.3, 1 );
 var green = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var copterBody = new THREE.Mesh( copterGeometry, green );
 
-var rotorGeometry = new THREE.BoxGeometry(0.2, 0.05, 0.2);
-var brown = new THREE.MeshBasicMaterial( { color: 0x30f03f } );
+var rotorGeometry = new THREE.BoxGeometry(0.3, 0.05, 0.05);
+var blue = new THREE.MeshBasicMaterial( { color: 0x6699FF } );
 var rotors = [];
 for(var i = 0; i < 4; i++)
-  rotors[i] = new THREE.Mesh( rotorGeometry, brown ).translateY(0.2);;
+  rotors[i] = new THREE.Mesh( rotorGeometry, blue ).translateY(0.175);;
 
 rotors[0].translateX(0.5);
 rotors[0].translateZ(0.5);
