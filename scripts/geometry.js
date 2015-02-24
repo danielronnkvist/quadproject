4bb2b08230098d1bea0fa04ad67fc620e84ddeb4
danielronnkvist <daniel@ronnkvist.com>
2015-02-24 18:29:10 +0100

    Moved the update function to a geometry object

--- scripts/geometry.js
+++ scripts/geometry.js
@@ -1,22 +1,38 @@
-var copterGeometry = new THREE.BoxGeometry( 1, 0.3, 1 );
-var green = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var copterBody = new THREE.Mesh( copterGeometry, green );
+var Copter = function(){
+  this.copterGeometry = new THREE.BoxGeometry( 1, 0.3, 1 );
+  this.green = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  this.copterBody = new THREE.Mesh( this.copterGeometry, this.green );
 
-var rotorGeometry = new THREE.BoxGeometry(0.3, 0.05, 0.05);
-var blue = new THREE.MeshBasicMaterial( { color: 0x6699FF } );
-var rotors = [];
-for(var i = 0; i < 4; i++)
-  rotors[i] = new THREE.Mesh( rotorGeometry, blue ).translateY(0.175);;
+  this.rotorGeometry = new THREE.BoxGeometry(0.3, 0.05, 0.05);
+  this.blue = new THREE.MeshBasicMaterial( { color: 0x6699FF } );
+  this.rotors = [];
+  for(var i = 0; i < 4; i++)
+    this.rotors[i] = new THREE.Mesh( this.rotorGeometry, this.blue ).translateY(0.175);;
 
-rotors[0].translateZ(-0.5);
+  this.rotors[0].translateZ(-0.5);
 
-rotors[1].translateX(0.5);
+  this.rotors[1].translateX(0.5);
 
-rotors[2].translateZ(0.5);
+  this.rotors[2].translateZ(0.5);
 
-rotors[3].translateX(-0.5);
+  this.rotors[3].translateX(-0.5);
 
-var copter = new THREE.Object3D();
-copter.add( copterBody );
-for(var i = 0; i < 4; i++)
-  copter.add( rotors[i] );
+  this.object = new THREE.Object3D();
+  this.object.add( this.copterBody );
+  for(var i = 0; i < 4; i++)
+    this.object.add( this.rotors[i] );
+}
+
+Copter.prototype.update = function(delta){
+  var pos = newPos(delta);
+
+  this.object.position.x = (pos[0]);
+  this.object.position.y = (pos[1]);
+  this.object.position.z = (pos[2]);
+
+  this.object.rotation.x = pos[3];
+  this.object.rotation.y = pos[4];
+  this.object.rotation.z = pos[5];
+
+  return pos;
+}
