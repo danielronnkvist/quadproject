1a7684e483801ff09e621cd3f8299f22ddafcb4b
danielronnkvist <daniel@ronnkvist.com>
2015-02-24 22:29:22 +0100

    Added redis, stores the copter whenever a new one comes in

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express'),
     app = express(),
     ejs = require('ejs'),
     http = require('http').Server(app),
-    io = require('socket.io')(http);
+    io = require('socket.io')(http),
+    redis = require('redis'),
+    client = redis.createClient(); // Creates a new client
 
 // EXPRESS CONFIG
 app.set('view engine', 'ejs');
@@ -10,11 +12,36 @@ app.set('port', process.env.PORT || 8000);
 app.use("/scripts",express.static(__dirname+"/scripts"));
 
 app.get('/', function(req, res){
-  res.render('index.ejs');
+  client.smembers('copters', function(err, reply){
+    console.log(reply)
+    res.render('index.ejs', {
+      copters: reply
+    });
+  });
+});
+
+client.on('connect', function() {
+  client.del('copters', function(err, reply){
+    if(err) console.log(err);
+    console.log("Succesfully reseted redis set");
+  });
+    console.log('connected to redis server');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
-});
+  socket.on('add copter', function(msg){
+    // console.log(msg);
+    client.sadd(['copters', JSON.stringify(msg)], function(err, reply){
+      if(!err){
+        console.log("Added copter to redis");
+        socket.emit('add copter', msg);
+      }
+    });
+  });
 
-http.listen(app.get('port'));
+
+});
+http.listen(app.get('port'), function(){
+  console.log("Server running on port %d", app.get('port'));
+});
